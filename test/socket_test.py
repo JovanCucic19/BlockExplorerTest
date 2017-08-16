38f845a94159ccb8f6a2c7a2b0eacfc2957d354b
MicicFilip <micic.filip@protonmail.com>
2017-08-16 15:39:30 +0200

    better mock

--- test/socket_test.py
+++ test/socket_test.py
@@ -215,6 +215,31 @@ class TestSocketIO(unittest.TestCase):
             mock_response.status_code = 200
             mock_response.content = received[0]['args'][0]
 
+            expected_block_data = {
+                "bits": "0x1b05c193",
+                "chainwork": "0x65bb3334c246beee",
+                "difficulty": "11385.21547576",
+                "hash": "db52e9d94210b0dbd22cca213275f0599c3a4c580ac9014b07fdd5b208779fd8",
+                "height": 1750461,
+                "merkleroot": "86feb4073bab1e9114a26baf0ec2aedd569e1fd86d7f3417ef45996d3de879c5",
+                "nextblockhash": None,
+                "nonce": 3047044099,
+                "previousblockhash": "3c972dce5207d0aca93de092b7f69340b5f3053ce4879818e16e12a38f55d35e",
+                "size": 1402,
+                "target": "0x5c193000000000000000000000000000000000000000000000000L",
+                "time": 1502881366,
+                "total": "366.34138225",
+                "tx": [
+                    "22b96e889dc272c307adf4878bb60e481fffcd4708be20598f72e486444536ee",
+                    "acceba9c589e9500b62376308c79d8e467d6a79601b5b1c4ac3e2da082d4fe11",
+                    "bf57bb76e08cdb62745aa99a9c6a34310422aadec03e9e6aee8083352d8d8c24",
+                    "96e249771366fa23ad332515130811aafb996ddc1d6a25c4f4bb30d0b361c525",
+                    "7bf516d3d3de2df073da9dddfb9aeca0840016b5ca26cde96c9209e5068c1203"
+                ],
+                "version": 2,
+                "work": 48899874279188
+            }
+
             response = get_data_from_url('http://blockexplorer.gamecredits.com/api/blocks/latest?limit=1')
 
             mock_hash = response.content['hash']
@@ -222,9 +247,9 @@ class TestSocketIO(unittest.TestCase):
             mock_nextblockhash = response.content['nextblockhash']
 
             self.assertEqual(response.status_code, 200)
-            self.assertEqual(response.content['hash'], check_typeof_parameter(mock_hash))
-            self.assertEqual(response.content['height'], check_typeof_parameter(mock_height))
-            self.assertEqual(response.content['nextblockhash'],
+            self.assertEqual(expected_block_data['hash'], check_typeof_parameter(mock_hash))
+            self.assertEqual(expected_block_data['height'], check_typeof_parameter(mock_height))
+            self.assertEqual(expected_block_data['nextblockhash'],
                              check_typeof_parameter(mock_nextblockhash))
 
     def test_mock_tx_data(self):
@@ -239,6 +264,36 @@ class TestSocketIO(unittest.TestCase):
             mock_response.status_code = 200
             mock_response.content = received[0]['args'][0]
 
+            expected_tx_data = {
+            "blockhash": "23c034d50ed039e22ac276652f788f237c98f3a028acc60288e0a1624b0d54b4",
+            "blocktime": 1502888665,
+            "locktime": 0,
+            "total": 12.5001,
+            "txid": "66b80940be43ff0aec5df5716c91257120b29354d70a58e07872eda1fa038b89",
+            "version": 1,
+            "vin": [
+              {
+                "coinbase": "0311b61a062f503253482f04d7429459080000002524000000352f737a62706f6f6c2ffabe6d6d6568d84c3da40e4816bd3dac64bf316e89f3d973126fc34fae0c022400a8fe180800000000000000",
+                "hex": None,
+                "prev_txid": None,
+                "sequence": 0,
+                "vout_index": None
+              }
+            ],
+            "vout": [
+              {
+                "addresses": [
+                  "GfVRuK6D4EvAG9KoxVhBEV6xwT6451Dwab"
+                ],
+                "asm": "OP_DUP OP_HASH160 ed72a54b8926cb13d522e186c884dbd5c45998b1 OP_EQUALVERIFY OP_CHECKSIG",
+                "reqSigs": 1,
+                "spent": False,
+                "type": "pubkeyhash",
+                "value": 12.5001
+              }
+            ]
+            }
+
             response = get_data_from_url('http://blockexplorer.gamecredits.com/api/transactions/latest?limit=1')
 
 
@@ -247,9 +302,9 @@ class TestSocketIO(unittest.TestCase):
             mock_total = response.content['total']
 
             self.assertEqual(response.status_code, 200)
-            self.assertEqual(response.content['blockhash'], check_typeof_parameter(mock_blockhash))
-            self.assertEqual(response.content['txid'], check_typeof_parameter(mock_txid))
-            self.assertEqual(response.content['total'], check_typeof_parameter(mock_total))
+            self.assertEqual(expected_tx_data['blockhash'], check_typeof_parameter(mock_blockhash))
+            self.assertEqual(expected_tx_data['txid'], check_typeof_parameter(mock_txid))
+            self.assertEqual(expected_tx_data['total'], check_typeof_parameter(mock_total))
 
 
 
